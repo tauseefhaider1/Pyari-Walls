fae1164af88d8c93c8648f494b433c17f93ffe36
Tauseef-haider <tauseefhaider659@gmail.comgit config --global user.name>
2025-08-12 17:11:03 +0500

    Update files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/my-project/', // add your repo name here
+
   plugins: [react()],
 })
